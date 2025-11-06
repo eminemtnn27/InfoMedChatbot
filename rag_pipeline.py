8aa48ce4bc8398a2cb7c092770a046361d3cfeb1
Emine Metin <eminemetin9@gmail.com>
2025-11-06 21:56:38 +0300

    Eksik bağımlılıklar requirements.txt dosyasına eklendi

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -7,7 +7,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_core.language_models.llms import LLM
-from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain.chains import RetrievalQA
   
 
 # --- Ortam değişkenleri ---
